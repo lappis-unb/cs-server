9b9c0134c1e728787761ced1b20595a73d72ac3f
Clarissa Borges <clarissalimab08@gmail.com>
2017-07-20 11:49:28 -0300

    Creates register pages with its fields
    Signed-off-by: Lucas Malta <lucas50xd@hotmail.com>
    Signed-off-by: Clarissa Borges <clarissalimab08@gmail.com>

--- frontend/src/Page/Register.elm
+++ frontend/src/Page/Register.elm
@@ -4,11 +4,35 @@ import Codeschool.Model exposing (Model)
 import Codeschool.Msg as Msg exposing (Msg)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Attributes exposing (..)
+import Polymer.Paper exposing (button)
 import Ui.Generic exposing (container)
 import Ui.Parts exposing (promoSimple, promoTable, simpleHero)
 
 
+regForm model field tp =
+    div []
+        [ div [ class "item-form" ]
+            [ input [ placeholder field, type_ tp ] []
+            ]
+        ]
+
+
 view : Model -> Html Msg
-view cls =
-    div [] [ text "hiiii" ]
+view model =
+    div []
+        [ simpleHero "Register" "" "simple-hero__page-blue"
+        , div [ class "main-container" ]
+            [ h1 [ class "form-title" ] [ text "Required Fields" ]
+            , regForm model "First Name" "text"
+            , regForm model "Last Name" "text"
+            , regForm model "Username" "text"
+            , regForm model "E-mail" "email"
+            , regForm model "Password" "password"
+            , regForm model "Repeat Password" "password"
+            , h1 [ class "form-title" ] [ text "Optional Fields" ]
+            , regForm model "Gender" "text"
+            , regForm model "Birthday" "date"
+            , regForm model "About me" "text"
+            , Polymer.Paper.button [ class "submit-button" ] [ text "Submit" ]
+            ]
+        ]
