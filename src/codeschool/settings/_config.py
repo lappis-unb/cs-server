783f049a2bfeab80dd1a96571bcee9f9c3ee4248
ThalissonMelo <tallisonmelo@hotmail.com>
2017-08-24 11:07:13 -0300

    Add login authentication using the api

--- src/codeschool/settings/_config.py
+++ src/codeschool/settings/_config.py
@@ -195,6 +195,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
 
 # Tries to detect if it is running on a test session
