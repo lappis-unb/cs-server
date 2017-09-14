2fa2d6bd8af58e0598a78a851f878d6319357ad1
joaorobson <joaorobsonmartins@gmail.com>
2017-09-14 10:27:17 -0300

    Add permissions to visualize users' informations

--- src/codeschool/core/users/permissions.py
+++ src/codeschool/core/users/permissions.py
@@ -1,16 +1,22 @@
 from rest_framework.permissions import BasePermission
-
-# https://github.com/HazyResearch/elementary/blob/master/django/resources/views.py
+from .models import *
+from .views import *
 
 class IsAdminOrSelf(BasePermission):
     def has_permission(self, request, view):
+
         if not request.user or not request.user.is_authenticated():
             return False
         if not request.user.is_staff:
+            all_info = view.queryset
+            owner_user_id = request.user.id
+            owner_user_info = User.objects.filter(id=owner_user_id)
+            view.queryset = owner_user_info
             return request.method in ['GET', 'HEAD', 'OPTIONS', 'PUT']
         return True
 
     def has_object_permission(self, request, view, obj):
+        print("haso")
         if request.user.is_staff:
             return True
         return request.user == obj
