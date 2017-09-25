a52c5edd87cbb8271becdb8ed7c894bd3b0b765f
joaorobson <joaorobsonmartins@gmail.com>
2017-09-25 09:07:21 -0300

    Change password route configuration

--- src/codeschool/core/users/api.py
+++ src/codeschool/core/users/api.py
@@ -1,5 +1,8 @@
 from codeschool.api import router
+from django.conf.urls import url
+
 from . import views
 
 router.register(r'users', views.UserViewSet,base_name="users")
 router.register(r'detail', views.UserDetailViewSet)
+router.register(r'change_password',views.ChangePasswordViewSet,base_name="change-password")
